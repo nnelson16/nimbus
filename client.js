45cc4e4f4c65f8c93a98ae4020a6cbccf778b904
nnelson16 <nn167@scarletmail.rutgers.edu>
2017-12-17 11:07:24 -0500

    another typo fixed

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 const dataFeatures = require('./data_features/datafeatures.js');
-const autocomplete = require('./autocomplete/autocomplete.js');
+const autoComplete = require('./autocomplete/autocomplete.js');
 module.exports = function () {
   var weatherClient = {};
   weatherClient.getDataFeaturesService = dataFeatures;
-  weatherClient.getAutoCompleteService = autocomplete;
+  weatherClient.getAutoCompleteService = autoComplete;
   return weatherClient;
 };
